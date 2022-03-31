4d0cd6550540887a97b7428b47f3eca3ac41a858
HeIIow2 <Lars.Noack@outlook.de>
2022-03-31 23:32:30 +0200

    solved

--- round2/Hex max/hex.py
+++ round2/Hex max/hex.py
@@ -5,11 +5,13 @@ from dataclasses import dataclass
 import numpy as np
 import copy
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.WARNING)
+
 
 class Hex(enum.Enum):
     add = "add"
     remove = "remove"
+
 
 # [zahl vorher][zahl nacher][add remove]
 hex_matrix = []
@@ -35,67 +37,9 @@ for hex_1, hex_matrix_1 in enumerate(hex_matrix):
                 remove += 1
             elif not bool1 and bool2:
                 add += 1
-            
+
         change_hex[hex_1][hex_2] = {Hex.add: add, Hex.remove: remove}
 
-@dataclass
-class Step:
-    hex_number: list
-    moves_left: int
-    free_adds: int
-    free_removes: int
-    last_move: int = 0
-    possible_moves = None
-    level: int = 0
-
-    def __lt__(self, other):
-        if self.free_adds or self.free_removes:
-            return True
-        if other.free_adds or other.free_removes:
-            return False
-
-        for digit1, digit2 in zip(self.hex_number, other.hex_number):
-            if digit1.digit != digit2.digit:
-                raise Exception("'hex_number' in {self} and {other} is not sorted")
-            if digit1.value != digit2.value:
-                return digit1.value < digit2.value
-
-    def skip_to_next_move(self):
-        self.last_move += 1
-        if self.last_move >= len(self.possible_moves) or self.possible_moves[self.last_move].digit == -1:
-            self.level += 1
-            self.possible_moves = np.sort(np.array([hex_digit.best_move(self.level, self.moves_left, self.free_adds, self.free_removes) for hex_digit in self.hex_number]))[::-1]
-        return True
-
-    def next_move(self):
-        # print(self.moves_left, self.free_adds, self.free_removes)
-        hex_number_ = self.hex_number.copy()
-        if self.possible_moves is None:
-            self.possible_moves = np.sort(np.array([hex_digit.best_move(self.level, self.moves_left, self.free_adds, self.free_removes) for hex_digit in self.hex_number]))[::-1]
-        
-        best_move = self.possible_moves[self.last_move]
-        if best_move.digit == -1:
-            return None
-
-        moves_left = self.moves_left - best_move.moves
-        """
-        free_adds = best_move.free_adds
-        free_removes = best_move.free_removes
-
-        moves_left -= best_move.moves
-        free_adds += best_move.remove
-        free_removes += best_move.add
-        """
-
-        self.last_move += 1
-
-        hex_number_[best_move.digit].execute_move(best_move)
-        logging.debug(f"executing {best_move} at {hex_number_[best_move.digit]}")
-
-        return Step(hex_number_, moves_left, best_move.free_adds, best_move.free_removes)
-
-    def print_num(self):
-        return "".join([f"{hex_digit.value:x}" for hex_digit in reversed(self.hex_number)])
 
 @dataclass
 class Move:
@@ -108,6 +52,7 @@ class Move:
     digit: int
     free_adds: int
     free_removes: int
+    moves_left: int
     base: int = 16
 
     def __lt__(self, other):
@@ -120,9 +65,10 @@ class Move:
         return self.digit < other.digit
         # return self.score * self.base**self.digit < other.score * other.base**other.digit
 
+
 # klasse einer Hex ziffer
 class HexDigit:
-    def __init__(self, value, digit: int, base: int=16):
+    def __init__(self, value, digit: int, base: int = 16):
         self.value = int(value, base)
         self.digit = digit
         self.base = base
@@ -140,7 +86,7 @@ class HexDigit:
             if add < 0:
                 free_adds = abs(add)
                 add = 0
-            
+
         if free_removes:
             remove -= free_removes
             if remove < 0:
@@ -150,25 +96,91 @@ class HexDigit:
         moves = max(add, remove)
 
         if add > remove:
-            free_removes += add-remove
+            free_removes += add - remove
         else:
-            free_adds += remove-add
+            free_adds += remove - add
 
-        if moves > moves_left:
-            return Move(-1, -1, -1, -1, -1, -1, -1, -1, -1)
+        moves_left -= moves
+
+        if moves_left < 0:
+            return None
+
+        if moves_left <= 0 and free_adds > 0:
+            return None
+        if moves_left <= 0 and free_removes > 0:
+            return None
 
         # +1 weil div by 0
-        score = i / (moves+1)
+        score = i / (moves + 1)
 
-        return Move(moves, add, remove, score, self.value, i, self.digit, free_adds, free_removes)
+        return Move(moves, add, remove, score, self.value, i, self.digit, free_adds, free_removes, moves_left)
 
     def best_move(self, level: int, moves_left: int, free_adds: int, free_removes: int):
-        moves = np.array([self.get_move(i, moves_left, free_adds, free_removes) for i in range(self.value+1, self.base)])
-        moves = np.sort(moves)[::-1] 
+        moves = np.array(
+            [self.get_move(i, moves_left, free_adds, free_removes) for i in range(self.value + 1, self.base)])
+        moves = np.sort(moves)[::-1]
         return moves[level]
+
+    def get_moves(self, moves_left: int, free_adds: int, free_removes: int):
+        return_moves = []
+        for i in range(self.value + 1, self.base):
+            temp_res = self.get_move(i, moves_left, free_adds, free_removes)
+            if temp_res is not None:
+                return_moves.append(temp_res)
+
+        return np.array(return_moves)
 
     def execute_move(self, move):
         self.value = move.to
+
+
+@dataclass
+class Step:
+    hex_number: list
+    moves_left: int
+    free_adds: int
+    free_removes: int
+    last_move: int = 0
+    possible_moves = None
+
+    def __str__(self):
+        return f"{self.print_num()} (moves: {self.moves_left}, adds: {self.free_adds}, removes: {self.free_removes})"
+
+    def __lt__(self, other):
+        if self.free_adds or self.free_removes:
+            return True
+        if other.free_adds or other.free_removes:
+            return False
+
+        for digit1, digit2 in zip(self.hex_number, other.hex_number):
+            if digit1.digit != digit2.digit:
+                raise Exception("'hex_number' in {self} and {other} is not sorted")
+            if digit1.value != digit2.value:
+                return digit1.value < digit2.value
+
+    def next_move(self):
+        # print(self.moves_left, self.free_adds, self.free_removes)
+        hex_number_ = copy.deepcopy(self.hex_number)
+        if self.possible_moves is None:
+            flattened_array = np.hstack(
+                [hex_digit.get_moves(self.moves_left, self.free_adds, self.free_removes) for hex_digit in
+                 self.hex_number])
+            self.possible_moves = np.sort(flattened_array)[::-1]
+
+        if self.last_move >= len(self.possible_moves):
+            return None
+
+        best_move = self.possible_moves[self.last_move]
+        self.last_move += 1
+
+        hex_number_[best_move.digit].execute_move(best_move)
+        logging.debug(f"executing {best_move} at {hex_number_[best_move.digit]}")
+
+        return Step(hex_number_, best_move.moves_left, best_move.free_adds, best_move.free_removes)
+
+    def print_num(self):
+        return "".join([f"{hex_digit.value:x}" for hex_digit in reversed(self.hex_number)])
+
 
 def main(EXAMPLE: int = 0):
     # read example file
@@ -189,30 +201,29 @@ def main(EXAMPLE: int = 0):
     biggest_history = copy.deepcopy(history)
 
     # just a timeout that I dont get stuck in an infinite loop
-    timeout = 1000
+    timeout = MOVES
     iteration = 0
     while history[-1].moves_left > 0 or history[-1].free_removes > 0 or history[-1].free_adds > 0:
+        iteration += 1
         new_move = history[-1].next_move()
-        if new_move is None:
-            while True:
-                if history[-1].skip_to_next_move():
-                    break
-                else:
-                    history.pop()
-                    if not len(history):
-                        print("huh")
-                        break
-                    iteration -= 1
-            
+        iteration += 1
+        while new_move is None:
+            history.pop()
+            iteration -= 1
+
+            if not len(history):
+                break
+
+            new_move = history[-1].next_move()
+
             # wenn es keine neue mögliche Zuege gibt, dann
             # breche ab und nehme das bisher beste ergebnis
             # moegliches Problem eines lokalen minnimums.
-            if not len(history):
-                print("huh")
-                break
-            continue
-        history.append(new_move)
+        if not len(history):
+            break
 
+        logging.info(new_move)
+        history.append(new_move)
 
         iteration += 1
         if iteration > timeout:
@@ -235,5 +246,6 @@ def main(EXAMPLE: int = 0):
     number = biggest_history[-1].print_num()
     logging.info(f"biggest Number: 0x{number}")
 
+
 if __name__ == "__main__":
-    main(0)
+    main(1)
