5f0ba0f781c6b5d08d9e77efcf24c813eca38915
HeIIow2 <Lars.Noack@outlook.de>
2022-04-07 19:24:25 +0200

    this is bs. need to go back to prev commit. I wanna die lol

--- round2/Hex_max/hex3.py
+++ round2/Hex_max/hex3.py
@@ -5,11 +5,13 @@ import enum
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
+
 class Hex(enum.Enum):
     add = "add"
     remove = "remove"
     moves = "moves"
     change = "change"
+
 
 # read hex.json and store as hex matrix
 """
@@ -43,11 +45,13 @@ for hex_1, hex_matrix_1 in enumerate(hex_matrix):
             elif not bool1 and bool2:
                 add += 1
 
-        change_hex[hex_1][hex_2] = {Hex.add: add, Hex.remove: remove, Hex.moves: max(add, remove), Hex.change: add - remove}
+        change_hex[hex_1][hex_2] = {Hex.add: add, Hex.remove: remove, Hex.moves: max(add, remove),
+                                    Hex.change: add - remove}
 
 
 def print_hex_list(hex_list: list):
     print(''.join([f"{digit:x}" for digit in hex_list]))
+
 
 def get_sticks(hex_str: str):
     sticks = 0
@@ -96,6 +100,7 @@ def get_biggest_hex_infinite(sticks: int, hex_length: int):
 
     return hex_number
 
+
 def needed_value_between_hex(hex_from: list, hex_to: list) -> (int, int):
     # get needed moves between two hex numbers
     total_added = 0
@@ -107,6 +112,7 @@ def needed_value_between_hex(hex_from: list, hex_to: list) -> (int, int):
 
     return total_added, total_removed
 
+
 def needed_moves_between_hex(hex_from: list, hex_to: list) -> int:
     # get needed moves between two hex numbers
     total_added, total_removed = needed_value_between_hex(hex_from, hex_to)
@@ -117,29 +123,42 @@ def needed_moves_between_hex(hex_from: list, hex_to: list) -> int:
 
     return max(total_added, total_removed)
 
+
+def get_needed_values_list(hex_from: list, hex_to: list) -> (list, list):
+    added = [change_hex[digit1][digit2][Hex.add] for digit1, digit2 in zip(hex_from, hex_to)]
+    removed = [change_hex[digit1][digit2][Hex.remove] for digit1, digit2 in zip(hex_from, hex_to)]
+
+    return added, removed
+
+def sum_except_one(list_: list, index: int) -> int:
+    return sum(list_[:index]) + sum(list_[index + 1:])
+
 def revert_illegal_moves(hex_from: list, hex_to_: list, max_moves: int) -> list:
     if len(hex_from) <= 0 or len(hex_to_) <= 0:
         raise Exception('hex_from or hex_to is empty. Thus the problem is not in revert_illegal_moves')
 
     hex_to = hex_to_.copy()
 
-    timeout = 5000
+    timeout = 10
     iteration = 0
     # the needed_moves in the while loop is performant enough running in about 5 seconds for 5000 iterations
-    needed_moves = 1
-    needed_change = 0
-    while needed_moves > 0 or needed_change != 0:
+    current_moves = max_moves + 1
+    current_change = 0
+
+    have_been = {}
+    for i in range(len(hex_from)):
+        have_been[i] = []
+
+    while current_moves > max_moves or current_change != 0:
         iteration += 1
         if iteration > timeout:
-            return hex_to
             print_hex_list(hex_to)
             raise Exception('Timeout while reverting moves')
 
-
-        needed_adds, needed_removes = needed_value_between_hex(hex_from, hex_to)
-        print(needed_adds, needed_removes)
-        needed_moves = max(needed_adds, needed_removes) - max_moves
-        needed_change = needed_adds - needed_removes
+        current_adds_list, current_removes_list = get_needed_values_list(hex_from, hex_to)
+        current_adds, current_removes = sum(current_adds_list), sum(current_removes_list)
+        current_change = current_adds - current_removes
+        current_moves = max(current_adds, current_removes)
 
         # print_hex_list(hex_to)
         # print(f"{needed_adds} - {needed_removes} = {needed_change} -> {needed_moves}")
@@ -149,67 +168,74 @@ def revert_illegal_moves(hex_from: list, hex_to_: list, max_moves: int) -> list:
                 # print(hex_from, hex_to, i)
                 continue
 
-            adds = change_hex[hex_from[i]][from_digit][Hex.add]
-            removes = change_hex[hex_from[i]][from_digit][Hex.remove]
-            # print(f"{i} {from_digit} specific a{adds} r{removes}")
+            particular_adds = sum_except_one(current_adds_list, i)
+            particular_removes = sum_except_one(current_removes_list, i)
 
-            if needed_change < 0:
-                if needed_change < adds:
-                    adds = 0
-                else:
-                    adds -= needed_change
-            elif needed_change > 0:
-                if needed_change > removes:
-                    removes = 0
-                else:
-                    removes += needed_change
-
-            moves = max(adds, removes)
-            changes = adds - removes
 
             # print(f"{i} {from_digit} move {needed_moves} change {needed_change} specific a{adds} r{removes} m{moves} c{changes}")
             new_digit = -1
             for to_digit in reversed(range(16)):
-                iter_adds = change_hex[hex_from[i]][to_digit][Hex.add]
-                iter_removes = change_hex[hex_from[i]][to_digit][Hex.remove]
+                # if to_digit == from_digit:
+                #     continue
+                # if to_digit in have_been[i]:
+                #     have_been[i].append(to_digit)
+                #     if len(have_been[i]) > 6:
+                #         have_been[i].pop(0)
+                #     continue
+                # have_been[i].append(to_digit)
+                # if len(have_been[i]) > 6:
+                #     have_been[i].pop(0)
 
+                change_adds = particular_adds + change_hex[hex_from[i]][to_digit][Hex.add]
+                change_removes = particular_removes + change_hex[hex_from[i]][to_digit][Hex.remove]
+                change_change = change_adds - change_removes
+                change_moves = max(change_adds, change_removes)
+                print()
+                if 0 < current_change and current_change < change_change:
+                    continue
+                if 0 > current_change and current_change > change_change:
+                    continue
 
-                if needed_change < 0:
-                    if needed_change < iter_adds:
-                        iter_adds = 0
-                    else:
-                        iter_adds -= needed_change
-                elif needed_change > 0:
-                    if needed_change > iter_removes:
-                        iter_removes = 0
-                    else:
-                        iter_removes += needed_change
+                if current_moves == change_moves:
+                    if current_adds > change_adds and current_removes == change_removes:
+                        new_digit = to_digit
+                        break
 
+                    if current_adds == change_adds and current_removes > change_removes:
+                        new_digit = to_digit
+                        break
+                    # if current_adds > change_adds:
+                    #     new_digit = to_digit
+                    #     break
+                    # if current_removes > change_removes:
+                    #     new_digit = to_digit
+                    #     break
+                    # if from_digit < to_digit:
+                    #     new_digit = to_digit
+                    #     break
 
-                iter_change = iter_adds - iter_removes
-                iter_moves = max(iter_adds, iter_removes)
+                    continue
 
-                # print(f"iter {i} {to_digit} add {iter_adds} remove {iter_removes} move {iter_moves} change {iter_change}")
-
-                if changes > 0 and iter_change > changes:   continue
-                if changes < 0 and iter_change < changes:   continue
-
-                if iter_moves < moves:
-                    if moves - iter_moves > needed_moves:
-                        continue
+                if current_moves > change_moves >= 0:
                     new_digit = to_digit
                     break
 
-            if new_digit != -1:
-                break
 
+            if new_digit != -1:
+                print("current")
+                print(current_adds, current_removes, current_change, current_moves)
+                print("changed")
+                print(change_adds, change_removes, change_change, change_moves)
+                print(f"changing {from_digit} to {to_digit} at digit {i}")
+                new_digit = to_digit
+                break
 
         if new_digit == -1:
             pass
             # print_hex_list(hex_to)
-            # logging.warning('No digit found to revert illegal moves (if it occurs only once, it is not an error)')
-        else:   hex_to[i] = new_digit
-
+            logging.warning('No digit found to revert illegal moves (if it occurs only once, it is not an error)')
+        else:
+            hex_to[i] = new_digit
 
     return hex_to
 
@@ -246,8 +272,7 @@ def execute_file(file_number: int = 0) -> list:
     return solution
 
 
-
 if __name__ == '__main__':
-    execute_file(4)
+    execute_file(0)
     # for elem in change_hex[15]:
     #     print(change_hex[15][elem])
